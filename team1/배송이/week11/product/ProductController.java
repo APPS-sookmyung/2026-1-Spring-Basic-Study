2336ee5b1600e38626f5115a204a61e4cfb18421
peisonger <realsonge@sookmyung.ac.kr>
2026-07-21 13:46:41 +0900

    [Fix] 11주차 스터디 파일 수정

---
+++ team1/배송이/week11/product/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.demo.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@ResponseBody
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    ProductController(){
+        System.out.println("진짜 되나..? 진짜 스프링이 알아서 객체 생성을 하나..?");
+    }
+
+    @RequestMapping(value="/products/{id}", method = RequestMethod.GET)
+    public Product getProduct(@PathVariable("id") int id){
+        return productService.getProduct(id);
+    }
+
+    @RequestMapping(value="/products", method = RequestMethod.POST)
+    public void saveProduct(@RequestBody Product product){
+        productService.saveProduct(product);
+    }
+}
